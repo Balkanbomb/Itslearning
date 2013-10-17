71423a5f5cbb6777946bdebd36e02b5d805285fc
Marcus Månsson <marcusmansson@yahoo.se>
2013-10-17 15:08:02 +0200

    Added Rodrigos work to master

---
+++ misc/layout/ExpandableListView2/src/com/example/expandablelistview/ExpandableListAdapter.java
@@ -0,0 +1,151 @@
+package com.example.expandablelistview;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.example.expandablelistview.R.color;
+ 
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+ 
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+ 
+    private Context _context;
+    private List<Article> _listDataHeader; // header titles
+    
+    // child data in format of header title, child title
+    private HashMap<Article, List<String>> _listDataChild;
+    
+    boolean showHeaderText = true;
+ 
+    public ExpandableListAdapter(Context context, List<Article> listDataHeader,
+            HashMap<Article, List<String>> listChildData) {
+        this._context = context;
+        this._listDataHeader = listDataHeader;
+        this._listDataChild = listChildData;
+    }
+ 
+    @Override
+    public Object getChild(int groupPosition, int childPosititon) {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition))
+                .get(childPosititon);
+    }
+ 
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+ 
+    @Override
+    public View getChildView(int groupPosition, final int childPosition,
+            boolean isLastChild, View convertView, ViewGroup parent) {
+ 
+        final String childText = (String) getChild(groupPosition, childPosition);
+ 
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.list_item, null);
+        }
+ 
+        TextView txtListChild = (TextView) convertView
+                .findViewById(R.id.lblListItem);
+ 
+        txtListChild.setText(childText);
+        return convertView;
+    }
+ 
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition))
+                .size();
+    }
+ 
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this._listDataHeader.get(groupPosition);
+    }
+ 
+    @Override
+    public int getGroupCount() {
+        return this._listDataHeader.size();
+    }
+ 
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+ 
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded,
+            View convertView, ViewGroup parent) {
+        Article headerTitle = (Article) getGroup(groupPosition);
+        
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.list_group, null);
+        }
+ 
+        TextView lblListHeader = (TextView) convertView
+                .findViewById(R.id.lblListHeader);
+       
+        lblListHeader.setText(headerTitle.getArticleHeader());
+        
+        TextView lblListHeaderDate = (TextView) convertView.findViewById(R.id.lblListHeaderDate);
+        	
+        lblListHeaderDate.setText(headerTitle.getArticleDate());
+        
+        TextView lblListHeaderText = (TextView) convertView.findViewById(R.id.lblListHeaderText);
+        
+        if (headerTitle.isTextVisible())
+        {
+        	lblListHeaderText.setVisibility(View.VISIBLE);
+        	lblListHeaderText.setText(headerTitle.getArticleText());
+        }
+        else
+        {
+        	lblListHeaderText.setVisibility(View.GONE);
+        	//lblListHeaderText.setText("");
+        }
+
+        
+        
+        
+        return convertView;
+    }
+ 
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+ 
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+
+	@Override
+	public void onGroupCollapsed(int groupPosition) {
+		// TODO Auto-generated method stub
+		super.onGroupCollapsed(groupPosition);
+		Article headerTitle = (Article) getGroup(groupPosition);
+		headerTitle.setTextVisible(true);
+		
+	}
+
+	@Override
+	public void onGroupExpanded(int groupPosition) {
+		// TODO Auto-generated method stub
+		super.onGroupExpanded(groupPosition);
+		Article headerTitle = (Article) getGroup(groupPosition);
+		headerTitle.setTextVisible(false);
+		
+	}
+}
--- misc/layout/ExpandableListView2/src/com/example/expandablelistview/MainActivity.java
+++ misc/layout/ExpandableListView2/src/com/example/expandablelistview/MainActivity.java
@@ -1,10 +1,9 @@
-package com.example.mad2013_itslearning;
+package com.example.expandablelistview;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import org.mcsoxford.rss.RSSItem;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.graphics.Color;
@@ -23,7 +22,7 @@ public class MainActivity extends Activity {
     ExpandableListAdapter listAdapter;
     ExpandableListView expListView;
     List<Article> listDataHeader;
-    HashMap<Article, String> listDataChild;
+    HashMap<Article, List<String>> listDataChild;
  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,19 +40,21 @@ public class MainActivity extends Activity {
        //custom ActionBar
         getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getActionBar().setCustomView(R.layout.abs_layout);
+        
+        
 
+		
+ 
         // get the listview
         expListView = (ExpandableListView) findViewById(R.id.lvExp);
  
         // preparing list data
         prepareListData();
  
-//        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
-        listAdapter = new ExpandableListAdapter(this, listDataHeader);
-         
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+ 
         // setting list adapter
         expListView.setAdapter(listAdapter);
-        
  
         // Listview Group click listener
         expListView.setOnGroupClickListener(new OnGroupClickListener() {
@@ -122,31 +123,30 @@ public class MainActivity extends Activity {
      */
     private void prepareListData() {
         listDataHeader = new ArrayList<Article>();
-//        listDataChild = new HashMap<Article, String>();
+        listDataChild = new HashMap<Article, List<String>>();
  
         // Adding child data
-
-        listDataHeader.add(new Article("New", "2013-10-15 10.30", "For this we will need your User stories and one or more of them will be put into the first sprint.", "330A"));
-        listDataHeader.add(new Article("Not so new", "2013-10-14 10.30", "We will need your User stories and one or more of them will be put into the first sprint.", "320B"));
-        listDataHeader.add(new Article("Old", "2013-10-13 10.30", "For this we will need your User stories.", "330A"));
-        listDataHeader.add(new Article("Oldest", "2013-10-12 10.30", "For this we will need your User stories.", "330A"));
-/*        
-        // Adding child data
-        String top250;
-        top250 = "For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint.";
-    
-        String nowShowing;
-        //nowShowing.add("For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint.");
-        nowShowing = "For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint.";
+                
+        listDataHeader.add(new Article("New", "2013-10-15 10.30", "For this we will need your User stories and one or more of them will be put into the first sprint."));
+        listDataHeader.add(new Article("Not so new", "2013-10-14 10.30", "We will need your User stories and one or more of them will be put into the first sprint."));
+        listDataHeader.add(new Article("Old", "2013-10-13 10.30", "For this we will need your User stories."));
+        listDataHeader.add(new Article("Oldest", "2013-10-12 10.30", "For this we will need your User stories."));
         
-        String comingSoon;
+        // Adding child data
+        List<String> top250 = new ArrayList<String>();
+        top250.add("For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint.");
+    
+        List<String> nowShowing = new ArrayList<String>();
+        //nowShowing.add("For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint.");
+        nowShowing.add("For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint.");
+        
+        List<String> comingSoon = new ArrayList<String>();
     //    comingSoon.add("For this we will need your User stories and one or more of them will be put into the first sprint.");
-        comingSoon = "For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint.For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint.";
+        comingSoon.add("For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint.For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint. For this we will need your User stories and oe or more of them will be put into the first sprint. For this we will need your User stories and one or more of them will be put into the first sprint.");
         
         listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
         listDataChild.put(listDataHeader.get(1), nowShowing);
         listDataChild.put(listDataHeader.get(2), comingSoon);
         listDataChild.put(listDataHeader.get(3), comingSoon);
- */
     }
 }
